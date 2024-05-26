13e63f7e95cdaee923da3246d9ce08682cf8e83f
Kobbysaah <102618159+Kobbysaah@users.noreply.github.com>
2024-05-26 14:05:30 +0000

    Update App.js
    Finishing with the font

--- App.js
+++ App.js
@@ -8,4 +8,17 @@ export default function App() {
     </View>
   );
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'lightblue',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+});
+
 
